897c6309746f83474489ce1845ac0e43312a57c1
Đoàn Thị Thu Quyên <thuquyentntl@gmail.com>
2024-09-02 19:54:07 +0700

    LED blink code simple

--- ESP_GPIO/main/gpio_example_main.c
+++ ESP_GPIO/main/gpio_example_main.c
@@ -1,5 +1,7 @@
 #include<stdio.h>
 #include"driver/gpio.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 void app_main(void){
     gpio_config_t io_config ={};
     io_config.pin_bit_mask = (1<< GPIO_NUM_2);
@@ -7,13 +9,12 @@ void app_main(void){
     io_config.pull_up_en = GPIO_PULLUP_DISABLE;
     io_config.pull_down_en = GPIO_PULLDOWN_DISABLE;
     io_config.intr_type = GPIO_INTR_DISABLE;
-    gpio_config(gpio_config);
+    gpio_config(&io_config);
 
     while(1){
         gpio_set_level(GPIO_NUM_2,1);
-        for (int i=0;i<1000;i++){
-            __asm("NOP");
-        }
+        vTaskDelay(50);
         gpio_set_level(GPIO_NUM_2,0);
+        vTaskDelay(50);
     }
 }
