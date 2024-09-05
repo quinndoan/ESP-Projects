7acd2455a85d35101bc6b1846e5ca88c4b299130
Đoàn Thị Thu Quyên <thuquyentntl@gmail.com>
2024-09-05 21:43:09 +0700

    Chuyển cho FreeRTOS.h lên đầu compile

--- ESP_GPIO_Button_And_LED/main/main.c
+++ ESP_GPIO_Button_And_LED/main/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
+#include "freertos/FreeRTOS.h"
 #include "freertos/queue.h"
 #include "driver/gpio.h"
-#include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_log.h"
 
